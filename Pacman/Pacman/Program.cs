4f72e1fefc2078a0ccdf82f43ec1f6993249bb26
JudyZhang <lianer0729@hotmail.com>
2016-12-06 13:57:42 +0100

    Add new project to game.

--- Pacman/Pacman/Program.cs
+++ Pacman/Pacman/Program.cs
@@ -9,39 +9,8 @@ namespace PacmanOutput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter function:");
-            var id = Console.ReadLine();
-            switch (id)
-            {
-                case "1":
-                    OutputEmptyStartegyList();
-                    break;
-                case "2":
-                    OutputStrategyArray();
-                    break;
-                default:
-                    Console.WriteLine("Enter the right number.");
-                    break;
-            }
-            Console.ReadKey();
         }
 
-        private static void OutputEmptyStartegyList()
-        {
-            var strategy = GenerateSituationArray.GetSituationArray();
-            for (var i=0; i< strategy.Length;i++)
-            {
-                Console.WriteLine($"{i}:{strategy[i]}");
-            }
-        }
-
-        private static void OutputStrategyArray()
-        {
-            var startegy = GenerateStartegy.FillRandomActionToSituation(GenerateSituationArray.GetSituationArray());
-            foreach (var l in startegy.Lines)
-            {
-                Console.WriteLine($"{l.Key}:{l.Value}");
-            }
-        }
+    
     }
 }
