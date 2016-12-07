2ba3f85ca021ab450333479ca96d62783b522e06
JudyZhang <lianer0729@hotmail.com>
2016-12-07 15:54:02 +0100

    Add Data folder and add new game process

---
+++ Pacman/Pacman/NextGeneration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonType;
+
+namespace PacmanGame
+{
+    public static class NextGeneration
+    {
+        public static void PlayGame(ref List<Pacman> pacmans)
+        {
+            
+        }  
+    }
+}
